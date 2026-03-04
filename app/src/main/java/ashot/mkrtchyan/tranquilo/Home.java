cfd6d07351a6dd2bd051c90650c218f8b41ec105
ashmkrtchian <ash.mkrtchian3@gmail.com>
2026-03-04 21:19:08 +0400

    Updated Home page layout and navigation

--- app/src/main/java/ashot/mkrtchyan/tranquilo/Home.java
+++ app/src/main/java/ashot/mkrtchyan/tranquilo/Home.java
@@ -6,51 +6,65 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Home extends AppCompatActivity {
 
     FirebaseAuth auth;
-    Button button;
-    TextView textView;
     FirebaseUser user;
+    TextView navHome, navMood, navProfile;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        navHome = findViewById(R.id.nav_home);
+        navMood = findViewById(R.id.nav_mood);
+        navProfile = findViewById(R.id.nav_profile);
 
         auth = FirebaseAuth.getInstance();
-        button = findViewById(R.id.logout);
-        textView = findViewById(R.id.user_details);
         user = auth.getCurrentUser();
         if (user == null) {
             Intent i = new Intent(getApplicationContext(), Login.class);
             startActivity(i);
             finish();
-        } else {
-            textView.setText(user.getEmail());
         }
+//        else {
+//            textView.setText(user.getEmail());
+//        }
 
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent i = new Intent(getApplicationContext(), Login.class);
-                startActivity(i);
-                finish();
-            }
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, new SecondFragment())
+                .commit();
+
+        navHome.setOnClickListener(v -> {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new SecondFragment())
+                    .commit();
         });
 
+        navMood.setOnClickListener(v -> {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new FirstFragment())
+                    .commit();
+        });
+
+        navProfile.setOnClickListener(v -> {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new ThirdFragment())
+                    .commit();
+        });
     }
 }
