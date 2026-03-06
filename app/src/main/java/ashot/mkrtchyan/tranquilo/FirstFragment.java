b77a1fb0130294119070e841fd809881aa6eedda
ashmkrtchian <ash.mkrtchian3@gmail.com>
2026-03-06 21:58:04 +0400

    Updated Home page layout and navigation

--- app/src/main/java/ashot/mkrtchyan/tranquilo/FirstFragment.java
+++ app/src/main/java/ashot/mkrtchyan/tranquilo/FirstFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +24,7 @@ public class FirstFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
 
     public FirstFragment() {
         // Required empty public constructor
@@ -60,5 +62,7 @@ public class FirstFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_first, container, false);
+
+
     }
 }
--- app/src/main/java/ashot/mkrtchyan/tranquilo/SecondFragment.java
+++ app/src/main/java/ashot/mkrtchyan/tranquilo/SecondFragment.java
@@ -1,5 +1,6 @@
 package ashot.mkrtchyan.tranquilo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,9 @@ public class SecondFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+    TextView music;
 
     public SecondFragment() {
         // Required empty public constructor
@@ -58,7 +63,16 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        music = view.findViewById(R.id.music);
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(), Music.class);
+                startActivity(i);
+            }
+        });
+        return view;
     }
 }
