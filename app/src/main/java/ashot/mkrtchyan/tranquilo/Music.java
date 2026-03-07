5ce048f314967e58eaa37260a622db40a4008859
ashmkrtchian <ash.mkrtchian3@gmail.com>
2026-03-07 09:49:07 +0400

    Add rain player and sounds

--- app/src/main/java/ashot/mkrtchyan/tranquilo/Music.java
+++ app/src/main/java/ashot/mkrtchyan/tranquilo/Music.java
@@ -1,6 +1,9 @@
 package ashot.mkrtchyan.tranquilo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,9 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Music extends AppCompatActivity {
 
+
+    FrameLayout rainCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        rainCard = findViewById(R.id.rainCard);
+        rainCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Rain.class);
+                startActivity(i);
+            }
+        });
     }
 }
